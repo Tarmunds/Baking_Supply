db05350dec557926ecaecc7b08e0468963bcf20b
Tarmunds <tarmund.pro@gmail.com>
2024-11-15 22:50:02 +0100

    Corrected the reimport of the addon

--- __init__.py
+++ __init__.py
@@ -1,3 +1,29 @@
+#  ***** GPL LICENSE BLOCK *****
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  All rights reserved.
+#  ***** GPL LICENSE BLOCK *****
+
+# <pep8 compliant>
+
+
+if "bpy" in locals():
+    import importlib
+    importlib.reload(BS_Panel)
+    importlib.reload(BS_Visibility)
+    importlib.reload(BS_Exporter)
+
 import bpy
 from . import BS_Panel
 from . import BS_Visibility
